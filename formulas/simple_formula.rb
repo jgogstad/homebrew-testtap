6e4c82019cb2b34472a31cbebe4154a60f35c043
Jostein Gogstad <jostein.gogstad@tapad.com>
2018-12-13 14:48:15 +0100

    Update simple_formula.rb to 0.1.0-SNAPSHOT

--- formulas/simple_formula.rb
+++ formulas/simple_formula.rb
@@ -2,13 +2,13 @@ class Tcloud < Formula
   desc "My test formula"
   homepage "https://github.com/Tapad/sbt-homebrew"
 
-  url "http://github.com/tapad/app-whateverr.zip"
-  sha256 "926aab256e741d988ce5682f5ab7a2d5de027be8c52b570b47342e2e9ef9b51f"
+  url "http://github.com/tapad/app-0.1.0-SNAPSHOT.zip"
+  sha256 "1d44650fd87f5f2174a5ffc1ad480eb5696c5e091120cf00de2cc87563633940"
 
   depends_on "curl" => "7.56.0"
 
   def install
-    system "make", "VERSION=whateverr"
+    system "make", "VERSION=0.1.0-SNAPSHOT"
     bin.install "myapp"
   end
 
