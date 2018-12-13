a9d36921f15caff1af2b266042ef191c3284bc51
Jostein Gogstad <jostein.gogstad@tapad.com>
2018-12-13 13:32:03 +0100

    Update simple_formula.rb to whatever

--- formulas/simple_formula.rb
+++ formulas/simple_formula.rb
@@ -2,15 +2,13 @@ class Tcloud < Formula
   desc "My test formula"
   homepage "https://github.com/Tapad/sbt-homebrew"
 
-  @@version = "0.1.0-SNAPSHOT"
-
-  url "http://github.com/tapad/tcloud-#{@@version}.zip"
-  sha256 "f8478effbe5e4ba110dea4d36d76ea55ca7b2f15e0c0d8195ea88a664a3a017c"
+  url "http://github.com/tapad/tcloud-whatever.zip"
+  sha256 "2a1e05edcfd313092fc661c6fab759f8f96f44de585ba301c08afd41414f1b26"
 
   depends_on "curl" => "7.56.0"
 
   def install
-    system "make", "SCALA_NATIVE_VERSION=0.3_2.11", "VERSION=#{@@version}", "BUILDPATH=#{buildpath}"
+    system "make", "SCALA_NATIVE_VERSION=0.3_2.11", "VERSION=whatever", "BUILDPATH=#{buildpath}"
     bin.install "tcloud"
     # system "make", "install", "PREFIX=#{prefix}"
   end
