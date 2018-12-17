0b2457465d24a9d79201cf77da688807d1bbf241
Jostein Gogstad <jostein.gogstad@gmail.com>
2018-12-18 00:09:09 +0100

    Update simple_formula.rb to 0.1.0-SNAPSHOT

--- formulas/simple_formula.rb
+++ formulas/simple_formula.rb
@@ -3,7 +3,7 @@ class Tcloud < Formula
   homepage "https://github.com/Tapad/sbt-homebrew"
 
   url "http://github.com/tapad/app-0.1.0-SNAPSHOT.zip"
-  sha256 "1d44650fd87f5f2174a5ffc1ad480eb5696c5e091120cf00de2cc87563633940"
+  sha256 "6400b7ecfcefd738f81c21110ab5fe697454ae91bf9bc284d7097ced17dcd673"
 
   depends_on "curl" => "7.56.0"
 
