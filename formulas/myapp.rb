5488fd62d6f2d383ec5f1d71504683a639cd6f51
Jostein Gogstad <jostein.gogstad@gmail.com>
2019-01-20 16:41:49 +0100

    Update myapp.rb to 0.1.1

--- formulas/myapp.rb
+++ formulas/myapp.rb
@@ -2,13 +2,13 @@ class Myapp < Formula
   desc "Scala Native Workshop"
   homepage "https://github.com/Tapad/scala-native-workshop"
 
-  url "http://localhost:8080/0.1.0-SNAPSHOT/zips/myapp.zip"
+  url "http://localhost:8080/0.1.1/zips/myapp.zip"
   sha256 "71cc50139c25fb41da1fa9cedee6f4f12c3cb0a7ec42ced33f3d4c2bcaf24b17"
 
   depends_on "curl" => "7.56.0"
 
   def install
-    system "make", "VERSION=0.1.0-SNAPSHOT", "BUILDPATH=#{buildpath}"
+    system "make", "VERSION=0.1.1", "BUILDPATH=#{buildpath}"
     bin.install "app"
   end
 
