d8ea9c3486cf315cebc5f8f56a87bfa79c23fa1b
Jostein Gogstad <jostein.gogstad@gmail.com>
2019-01-20 16:30:56 +0100

    Update myapp.rb to 0.1.0-SNAPSHOT

--- formulas/myapp.rb
+++ formulas/myapp.rb
@@ -3,7 +3,7 @@ class Myapp < Formula
   homepage "https://github.com/Tapad/scala-native-workshop"
 
   url "http://localhost:8080/0.1.0-SNAPSHOT/zips/myapp.zip"
-  sha256 ""
+  sha256 "71cc50139c25fb41da1fa9cedee6f4f12c3cb0a7ec42ced33f3d4c2bcaf24b17"
 
   depends_on "curl" => "7.56.0"
 
