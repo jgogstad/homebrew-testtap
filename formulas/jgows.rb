2e1b2c151203d9c1e6dfd79b47d39c1bf545aa66
Jostein Gogstad <jostein.gogstad@gmail.com>
2019-02-03 15:36:28 +0100

    Update jgows.rb to 0.1.2

--- formulas/jgows.rb
+++ formulas/jgows.rb
@@ -2,13 +2,14 @@ class Jgows < Formula
   desc "Scala Native Workshop"
   homepage "https://github.com/Tapad/scala-native-workshop"
 
-  url "https://nexus.tapad.com/repository/releases/com/tapad/workshop/jgows/11/jgows-11.zip"
-  sha256 "79b597484e5b6920312c140522e184b0046f3c360b1a4d785eff63dc6feec414"
+  url "https://nexus.tapad.com/repository/releases/com/tapad/workshop/jgows/0.1.2/jgows-0.1.2.zip"
+  sha256 "aa58742beba36f3b00a4124e4b852671d8616acc256c532ed0ef18bb36a51546"
+  version "0.1.2"
 
   depends_on "curl" => "7.56.0"
 
   def install
-    system "make", "VERSION=11", "BUILDPATH=#{buildpath}"
+    system "make", "VERSION=0.1.2", "BUILDPATH=#{buildpath}"
     bin.install "app"
   end
 
