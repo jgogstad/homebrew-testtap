4df600dc0be96ab06e069a39988264a819677fce
Jostein Gogstad <jostein.gogstad@gmail.com>
2019-02-03 16:05:49 +0100

    Update jgows.rb to 0.1.3

--- formulas/jgows.rb
+++ formulas/jgows.rb
@@ -2,14 +2,30 @@ class Jgows < Formula
   desc "Scala Native Workshop"
   homepage "https://github.com/Tapad/scala-native-workshop"
 
-  url "https://nexus.tapad.com/repository/releases/com/tapad/workshop/jgows/0.1.2/jgows-0.1.2.zip"
-  sha256 "aa58742beba36f3b00a4124e4b852671d8616acc256c532ed0ef18bb36a51546"
-  version "0.1.2"
+  credentials_file = "#{ENV['HOME']}/.ivy2/.credentials"
+  got_ivy_credentials = File.file?(credentials_file)
+  if !got_ivy_credentials
+    abort("Please provide your Nexus credentials in #{credentials_file}")
+  end
+
+  username=File.readlines(credentials_file).map { |line| if line =~ /user=(.+)/i; $1; end }.find{|x|!x.nil?}
+  password=File.readlines(credentials_file).map { |line| if line =~ /password=(.+)/i; $1; end }.find{|x|!x.nil?}
+
+  if username.nil? || password.nil?
+    abort("No credentials found in #{credentials_file}")
+  end
+
+  username_encoded = URI.encode(username)
+  password_encoded = URI.encode(password)
+
+  url "https://#{username}:#{password}@nexus.tapad.com/repository/releases/com/tapad/workshop/jgows/0.1.3/jgows-0.1.3.zip"
+  sha256 "b418ed2db5b4ca976b0bf5147298dc456c12fdb7eb5aa2e3638ab2fc0956967b"
+  version "0.1.3"
 
   depends_on "curl" => "7.56.0"
 
   def install
-    system "make", "VERSION=0.1.2", "BUILDPATH=#{buildpath}"
+    system "make", "VERSION=0.1.3", "BUILDPATH=#{buildpath}"
     bin.install "app"
   end
 
