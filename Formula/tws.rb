d9b237100faae5b9ad5083310b9ff7b8a68f5122
Jostein Gogstad <jostein.gogstad@gmail.com>
2019-02-08 14:22:07 +0100

    Update tws.rb to 0.0.11

--- Formula/tws.rb
+++ Formula/tws.rb
@@ -15,10 +15,10 @@ class Tws < Formula
     abort("No credentials found in #{@@credentials_file}")
   end
 
-  url "https://#{username}:#{password}@nexus.tapad.com/repository/releases/com/tapad/workshop/tws/0.0.10/tws-0.0.10.zip"
+  url "https://#{username}:#{password}@nexus.tapad.com/repository/releases/com/tapad/workshop/tws/0.0.11/tws-0.0.11.zip"
 
   sha256 "0ce1577618d87a4666fd9655f9016df34885d860d89f0898f19428ec0b7871fc"
-  version "0.0.10"
+  version "0.0.11"
 
   depends_on "curl" => "7.56.0"
   depends_on "llvm" => :build
@@ -27,12 +27,12 @@ class Tws < Formula
   depends_on "libidn"
 
   def install
-    system "make", "VERSION=0.0.10", "BUILDPATH=#{buildpath}", "CREDENTIALS=#{@@credentials_file}"
+    system "make", "VERSION=0.0.11", "BUILDPATH=#{buildpath}", "CREDENTIALS=#{@@credentials_file}"
     bin.install "tws"
   end
 
   bottle do
-    root_url "https://#{username}:#{password}@nexus.tapad.com/repository/releases/com/tapad/workshop/tws/0.0.10/"
+    root_url "https://#{username}:#{password}@nexus.tapad.com/repository/releases/com/tapad/workshop/tws/0.0.11/"
     cellar :any
     sha256 "2bb85c45f2e017cfcc931160a8b7a2e832a7cab11db8b63e2d3611c82348d3fb" => :high_sierra
   end
