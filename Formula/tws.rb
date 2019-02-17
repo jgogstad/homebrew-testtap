19b8d36a74559b23138afa8e57b1179c778d5c82
Jostein Gogstad <jostein.gogstad@gmail.com>
2019-02-17 11:38:09 +0100

    tws: add 0.0.12 bottle.

--- Formula/tws.rb
+++ Formula/tws.rb
@@ -7,6 +7,12 @@ class Tws < Formula
   sha256 "934765e48986260925a8b74f9b251c0f3359531703ff157549973cc78575b5d4"
   version "0.0.12"
 
+  bottle do
+    root_url "https://dl.bintray.com/jgogstadorg/testtap-bottles"
+    cellar :any
+    sha256 "ef80eaf2348146b933fa81c6808b1cef49d15d93ff542fb368ecee9cafac7beb" => :high_sierra
+  end
+
   depends_on "curl"
   depends_on "libidn"
   depends_on "llvm" => :build
