9592a62a7f91dab647e812e950450dfc42da7bde
Jostein Gogstad <jostein.gogstad@gmail.com>
2019-02-18 22:46:17 +0100

    tws-v0.5.1

--- Formula/tws.rb
+++ Formula/tws.rb
@@ -2,16 +2,10 @@ class Tws < Formula
   desc "Scala Native Workshop for tws"
   homepage "https://github.com/Tapad/scala-native-workshop"
 
-  url "http://localhost:8080/tws/0.5.0/zips/tws.zip"
+  url "http://localhost:8080/tws/0.5.1/zips/tws.zip"
 
-  version "0.5.0"
-  sha256 "06d0ced4221678b1118bbf0fb28b5f209595cce9e815476a7f5a0825f4f6920b"
-
-  bottle do
-    root_url "https://dl.bintray.com/jgogstad/bottles-testtap"
-    cellar :any
-    sha256 "5f9ecc5545f41758dae60aab40a4995d6eb55a659e38e704d9626c361bdc2b72" => :high_sierra
-  end
+  version "0.5.1"
+  sha256 "849bcb182f3ff62ced45c8d40e920d86e5dacb0a9f6bbeaf6208932ef02dbf7c"
 
   depends_on "bdw-gc" => :build
   depends_on "llvm" => :build
@@ -20,7 +14,7 @@ class Tws < Formula
   depends_on "re2"
 
   def install
-      system "make", "VERSION=0.5.0", "BUILDPATH=#{buildpath}"
+      system "make", "VERSION=0.5.1", "BUILDPATH=#{buildpath}"
       bin.install "tws"
   end
 end
